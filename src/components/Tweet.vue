4e427ab51c97bb9583e6e16e84581180333c537a
yuki fujimoto <tenriyu0416@gmail.com>
2023-07-15 16:46:29 +0900

    second

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import {ref} from 'vue'
-const tweets=ref([{id:'0',discription:'hell'},{id:1, discription:'no'}]);
-const inputtingDescription=ref<string>();
+const tweets = ref([{ id: 0, description: 'hell' }, { id: 1, description: 'no' }]);
+const inputtingDescription=ref<string>('');
 
 const postTweet=()=>{
 
-  console.log(inputtingDescription.value);
+  const tweet={id:Math.random(),description:inputtingDescription.value};
+  tweets.value.push(tweet);
+ 
+}
+
+const deleteTweet=(id:number)=>{
+  tweets.value=tweets.value.filter(t=>t.id !==id)
 }
 </script>
 
@@ -13,13 +19,15 @@ const postTweet=()=>{
   <div class="container">
     <h1>Tweeter</h1>
     <div class="form-container">
-      <input v-model="inputtingDescription"/>
+      <input v-model="inputtingDescription" />
       <button class="save-button" @click="postTweet">post</button>
     </div>
     <div  class="tweet-container">
+      <p v-show="tweets.length <= 0">No twwets have been added</p>
       <ul>
         <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
-          <span class="tweet-list">{{ tweet.discription }}</span>
+          <span class="tweet-list">{{ tweet.description }}</span>
+          <button class="delete-button" @click="deleteTweet(tweet.id)">delete</button>
         </li>
 
       </ul>
@@ -32,9 +40,31 @@ const postTweet=()=>{
   color:red;
 }
 .save-button{
-  color:blue;
-width:70px;
+  color:fff;
+  font-weight:bold;
+  background-color:#68c9c9;
+  border-radius:2px;
+  border:none;
+  width:60px;
+  height:22px;
 }
+.delete-button{
+  color:fff;
+  font-weight:bold;
+  background-color:#c99a68;
+  border-radius:2px;
+  border:none;
+  width:60px;
+  height:22px;
+}
+.save-button:hover{
+  background-color:#37bdbd;
+}
+.delete-button:hover{
+  background-color:#ac783f;
+}
+
+
 .container{
   display: flex;
   flex-direction: column;
@@ -43,6 +73,13 @@ width:70px;
 
 .tweet-list{
   list-style:none;
+  margin-bottom:12px;
+  border-radius:4px;
+  font-size:20px;
+  display:flex;
+  background-color:rgb(204,219,233);
+  padding: 8px 20px;
+
 }
 .form-container{
     display: flex;
