90e6c0a97ee167a7d2860216013654e3457336a0
yuki fujimoto <tenriyu0416@gmail.com>
2023-07-15 14:40:25 +0900

    first

---
+++ src/components/Payment.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <label>Payment.js</label>
+
+</template>
+
+<style scoped>
+label{
+  font-size:20px;
+}
+
+</style>
---
+++ src/components/Tweet.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+const tweets=ref([{id:'0',discription:'hell'},{id:1, discription:'no'}]);
+const inputtingDescription=ref<string>();
+
+const postTweet=()=>{
+
+  console.log(inputtingDescription.value);
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+      <input v-model="inputtingDescription"/>
+      <button class="save-button" @click="postTweet">post</button>
+    </div>
+    <div  class="tweet-container">
+      <ul>
+        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+          <span class="tweet-list">{{ tweet.discription }}</span>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style >
+.ok{
+  color:red;
+}
+.save-button{
+  color:blue;
+width:70px;
+}
+.container{
+  display: flex;
+  flex-direction: column;
+   align-items: center;
+}
+
+.tweet-list{
+  list-style:none;
+}
+.form-container{
+    display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+
+</style>
